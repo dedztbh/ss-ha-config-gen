07e1806b6db8489ff37ebbe7d2e17a0e2f345c8b
Peiqi Liu <peiqial@outlook.com>
2022-10-03 13:25:16 +0800

    using cloudflare speed test

--- haproxy.py
+++ haproxy.py
@@ -1,45 +1,80 @@
 import random
-import ipaddress
+# import ipaddress
+import sys
+from os.path import exists
+# import geoip2.database
 
-ranges = \
-    """173.245.48.0/20
-    103.21.244.0/22
-    103.22.200.0/22
-    103.31.4.0/22
-    141.101.64.0/18
-    108.162.192.0/18
-    190.93.240.0/20
-    188.114.96.0/20
-    197.234.240.0/22
-    198.41.128.0/17
-    162.158.0.0/15
-    104.16.0.0/12
-    172.64.0.0/13
-    131.0.72.0/22""".split()
-num_server = 500
+# ranges = \
+# """173.245.48.0/20
+# 103.21.244.0/22
+# 103.22.200.0/22
+# 103.31.4.0/22
+# 141.101.64.0/18
+# 108.162.192.0/18
+# 190.93.240.0/20
+# 188.114.96.0/20
+# 197.234.240.0/22
+# 198.41.128.0/17
+# 162.158.0.0/15
+# 104.16.0.0/13
+# 104.24.0.0/14
+# 172.64.0.0/13
+# 131.0.72.0/22""".split()
+# ranges = ['172.64.0.0/13']
+# _, num_server = sys.argv
+# num_server = int(num_server)
 
-ips = []
-for iprange in ranges:
-    for ip in ipaddress.IPv4Network(iprange):
-        ips.append(str(ip))
+# if not exists('ips.txt'):
+#     with open('ips.txt', 'w') as f:
+#         for iprange in ranges:
+#             for ip in ipaddress.IPv4Network(iprange):
+#                 f.write(str(ip) + '\n')
+
+# with open('ips.txt', 'r') as f:
+#     ips = f.readlines()
+#     ips = [ip.strip() for ip in ips]
+
+# ips = random.choices(ips, k=200000)
+#
+# with geoip2.database.Reader('./GeoLite2-Country.mmdb') as reader:
+#     def f(ip):
+#         try:
+#             code = reader.country(ip).country.iso_code
+#             if code == 'HK' or code == 'CN' or code == 'MO':
+#                 print(code)
+#                 return True
+#             return False
+#         except:
+#             return False
+#     ips = list(filter(f, ips))
+#
+# print(f'len(ips)={len(ips)}')
+
+with open("result.csv", encoding='utf-8') as f:
+    ips = [row.split(',')[0] for row in f.readlines()[1:]]
+
+# ips = random.choices(ips, k=min(num_server, len(ips)))
 
 config = ''
-for i, ip in enumerate(random.choices(ips, k=num_server)):
-    config += '    server server' + str(i) + ' ' + ip + ':443 check inter 1000 rise 3 fall 1\n'
+for i, ip in enumerate(ips):
+    config += '    server server' + \
+        str(i) + ' ' + ip.strip() + ':443 check observe layer4\n'
 
 config = \
-"""global
+    """global
     log 127.0.0.1 local0 notice
-    daemon
-    nbproc 8
 
 defaults
     log global
     mode tcp
-    balance source
-    timeout connect 3s
-    timeout client 1m
-    timeout server 1m
+    balance roundrobin
+    timeout connect 5s
+    timeout client 5s
+    timeout server 5s
+    option redispatch
+    option dontlognull
+    retries 3
+    maxconn 20480
 
 frontend main
     bind *:40498
--- win.py
+++ win.py
@@ -3,33 +3,39 @@ import json
 import ipaddress
 import copy
 
-ranges = \
-    """173.245.48.0/20
-    103.21.244.0/22
-    103.22.200.0/22
-    103.31.4.0/22
-    141.101.64.0/18
-    108.162.192.0/18
-    190.93.240.0/20
-    188.114.96.0/20
-    197.234.240.0/22
-    198.41.128.0/17
-    162.158.0.0/15
-    104.16.0.0/12
-    172.64.0.0/13
-    131.0.72.0/22""".split()
+# ranges = \
+# """173.245.48.0/20
+# 103.21.244.0/22
+# 103.22.200.0/22
+# 103.31.4.0/22
+# 141.101.64.0/18
+# 108.162.192.0/18
+# 190.93.240.0/20
+# 188.114.96.0/20
+# 197.234.240.0/22
+# 198.41.128.0/17
+# 162.158.0.0/15
+# 104.16.0.0/13
+# 104.24.0.0/14
+# 172.64.0.0/13
+# 131.0.72.0/22""".split()
 # ranges = ['104.16.0.0/12']
-num_server = 500
+# num_server = 500
 full_config = {
     "strategy": "com.shadowsocks.strategy.ha",
     "index": -1,
     "portableMode": True
 }
 
-ips = []
-for iprange in ranges:
-    for ip in ipaddress.IPv4Network(iprange):
-        ips.append(str(ip))
+# ips = []
+# for iprange in ranges:
+#     for ip in ipaddress.IPv4Network(iprange):
+#         ips.append(str(ip))
+
+ips = ['172.64.91.168', 'cloudflare.com']
+
+with open("..\\CloudflareST_windows_amd64\\result_hosts.txt", encoding='utf-8') as f:
+    ips += [row.split(',')[0] for row in f.readlines()[1:]]
 
 with open('template.json', 'r') as f:
     template = json.load(f)
@@ -40,8 +46,9 @@ def mk_new_server(ip):
     template0['server'] = ip
     return template0
 
+# ips = random.choices(ips, k=num_server)
 
-configs = list(map(mk_new_server, random.choices(ips, k=num_server)))
+configs = list(map(mk_new_server, ips))
 full_config['configs'] = configs
 
 with open('gui-config.json', 'w+') as f:
